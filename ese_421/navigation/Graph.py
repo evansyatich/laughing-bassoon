5d54fea59f854b5931960ce917b936bf1e2461f6
evansyatich <eyatich@seas.upenn.edu>
2018-10-08 21:08:46 -0400

    graph ready

--- ese_421/navigation/Graph.py
+++ ese_421/navigation/Graph.py
@@ -55,6 +55,26 @@ class Graph(object):
                 if {neighbour, vertex} not in edges:
                     edges.append({vertex, neighbour})
         return edges
+        
+    def find_path(self, start_vertex, end_vertex, path=None):
+        """ find a path from start_vertex to end_vertex 
+            in graph """
+        if path == None:
+            path = []
+        graph = self.__graph_dict
+        path = path + [start_vertex]
+        if start_vertex == end_vertex:
+            return path
+        if start_vertex not in graph:
+            return None
+        for vertex in graph[start_vertex]:
+            if vertex not in path:
+                extended_path = self.find_path(vertex, 
+                                               end_vertex, 
+                                               path)
+                if extended_path: 
+                    return extended_path
+        return None
 
     def __str__(self):
         res = "vertices: "
